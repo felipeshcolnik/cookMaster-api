3fa417c4d1443557103acccf620a20cb63d175c3
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 16:01:32 -0300

    newUser ftn service structure without validations

---
+++ src/services/user.js
@@ -0,0 +1,10 @@
+const model = require('../models/user');
+
+const newUser = async (name, email, password, role) => {
+  const result = await model.newUser(name, email, password, role);
+  return result;
+};
+
+module.export = {
+  newUser,
+};
