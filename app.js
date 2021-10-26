b3fb2c96603b445846e264115cfed1dedfaba3a6
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 16:41:56 -0300

    include post route to createUser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const user = require('./src/controllers/user');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+app.post('/user', user.newUser);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
