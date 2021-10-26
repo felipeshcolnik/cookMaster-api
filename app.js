02bd4994823fa89c23f72cf5c40d5550253d1a40
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 18:31:26 -0300

    include app.use(error) on app

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 const user = require('./src/controllers/user');
+const error = require('./src/utils/errorMidleware');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/user', user.newUser);
+app.post('/user', rescue(user.newUser));
+
+app.use(error);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
