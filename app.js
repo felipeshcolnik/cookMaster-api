2581528eac9cbbc172d66f1cbd40a791127f8e6f
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 15:21:37 -0300

    create app.js and set connection PORT

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
