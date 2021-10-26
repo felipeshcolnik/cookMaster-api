d0795d17432c3d04170d0df64a4a282eedd748ce
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 18:40:01 -0300

    takeOff user password of answer

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,14 @@
 const connection = require('./connection');
 
-const findUser = async (email) => {
+const findUser = async (email) => 
   connection()
     .then((db) => db.collection('users').findOne({ email }))
     .then((result) => result);
-};
 
 const newUser = async (name, email, password, role) => 
    connection()
     .then((db) => db.collection('users').insertOne({ name, email, password, role }))
-    .then((result) => ({ id: result.insertedId, name, email, password, role }));
+    .then((result) => ({ id: result.insertedId, name, email, role }));
 
 module.exports = { 
   findUser,
