b155af8cfd3b12d29e3ec650261bb835146fd873
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 18:30:26 -0300

    insert validade fct on newUser service

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,10 @@
 const model = require('../models/user');
+const validate = require('../utils/validationFcts');
 
 const newUser = async (name, email, password, role) => {
+  validate.newUserBody(name, email, password);
   const result = await model.newUser(name, email, password, role);
+  console.log(result);
   return result;
 };
 
