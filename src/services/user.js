6f7185c77d3863f7d1eeeeecbff72d31de5df9b6
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-26 18:54:47 -0300

    include userExist fct on service

--- src/services/user.js
+++ src/services/user.js
@@ -1,13 +1,24 @@
 const model = require('../models/user');
 const validate = require('../utils/validationFcts');
 
+const findUser = async (email) => {
+  const user = await model.findUser(email);
+  return user;
+};
+
 const newUser = async (name, email, password, role) => {
   validate.newUserBody(name, email, password);
+  const userAlreadyExists = await findUser(email);
+  if (userAlreadyExists) {
+    const error = new Error('Email already registered');
+    error.status = '409';
+    throw error;
+  }
   const result = await model.newUser(name, email, password, role);
-  console.log(result);
   return result;
 };
 
 module.exports = {
   newUser,
+  findUser,
 };
